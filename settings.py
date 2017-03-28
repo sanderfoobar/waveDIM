7742dd7ac7a808243a6603ff905671a014b607f4
Evert Arends <evert.arends98@gmail.com>
2017-03-28 13:15:53 +0200

    Updated stream url Arrow

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ streams = {
     "arrownl": {
         "id": "arrownl",
         "title": "Arrow Classic Rock",
-        "stream": "http://91.221.151.155:80",
+        "stream": "http://91.221.151.155:80/stream",
         "referer": "player.arrow.nl",
         "user-agent": "Mozilla 4",
         "mime": "audio/mpeg"
